d6c3a6c343ab0b749e0c986a1341ce824831c14d
yanfang chen <juliechen209@gmail.com>
2024-11-06 15:06:17 -0600

    add upsert and getpost methods for postscontroller

--- DotnetAPI/Controllers/PostsController.cs
+++ DotnetAPI/Controllers/PostsController.cs
@@ -1,11 +1,14 @@
+using System.Security.Claims;
 using DotnetAPI.Data;
 using DotnetAPI.Model;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotnetAPI.Controllers;
-
-
+namespace DotnetAPI.Controllers
+{
+    /// <summary>
+    /// Controller for handling post-related operations, including creating, updating, retrieving, and deleting posts.
+    /// </summary>
     [Authorize]
     [ApiController]
     [Route("[controller]")]
@@ -14,7 +17,7 @@ namespace DotnetAPI.Controllers;
         private readonly DataContextDapper _dapper;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="PostController"/> class.
+        /// Initializes a new instance of the <see cref="PostsController"/> class.
         /// </summary>
         /// <param name="config">Configuration object for database connection.</param>
         public PostsController(IConfiguration config) 
@@ -23,32 +26,43 @@ namespace DotnetAPI.Controllers;
         }
 
         /// <summary>
-        /// Adds a new post to the database.
+        /// Upserts (inserts or updates) a post. 
+        /// If PostId is provided and greater than 0, checks if it exists, and updates the post if found; otherwise, adds a new post.
+        /// If PostId is not provided or null, a new post is created.
         /// </summary>
-        /// <param name="postToAdd">DTO containing post details.</param>
-        /// <returns>Status of the addition operation.</returns>
-        [HttpPost("UpsertPost")]
-        public IActionResult UpsertPost(PostToAddDto postToAdd) 
+        /// <param name="postToUpsert">The post object to be upserted.</param>
+        /// <returns>Status indicating success or failure of the operation.</returns>
+        [HttpPut("UpsertPost")]
+        public IActionResult UpsertPost(Post postToUpsert) 
         {
-            string sql = @"
-            INSERT INTO DotnetAPISchema.Posts(
-                [UserId],
-                [PostTitle],
-                [PostContent],
-                [PostCreated],
-                [PostUpdated]) VALUES (" + this.User.FindFirst("userId")?.Value
-                + ",'" + postToAdd.PostTitle
-                + "','" + postToAdd.PostContent
-                + "', GETDATE(), GETDATE() )";
+            string sql = @"EXEC DotnetAPISchema.spPosts_Upsert
+             @PostId = @PostIdParam, 
+             @UserId = @UserIdParam, 
+             @PostTitle = @PostTitleParam, 
+             @PostContent = @PostContentParam";
 
-            if (_dapper.ExecuteSql(sql)) 
+            var parameters = new {
+                PostIdParam = postToUpsert.PostId > 0 ? postToUpsert.PostId : (int?)null,
+                UserIdParam = this.User.FindFirst("userId")?.Value,
+                PostTitleParam = postToUpsert.PostTitle,
+                PostContentParam = postToUpsert.PostContent
+            };
+
+            if (_dapper.ExecuteSqlWithParameter(sql, parameters)) 
             {
-                return Ok(postToAdd);
+                return Ok();
             }
-            throw new Exception("Failed to create a new post!");
+            throw new Exception("Failed to update/create a post!");
         }
 
-
+        /// <summary>
+        /// Retrieves posts based on optional PostId, UserId, and search parameters.
+        /// Provides default values to allow Swagger UI to generate a default input.
+        /// </summary>
+        /// <param name="postId">The ID of the post to retrieve. Default is 0.</param>
+        /// <param name="userId">The ID of the user who created the posts to retrieve. Default is 0.</param>
+        /// <param name="searchParam">Search keyword for post content or title. Default is "None".</param>
+        /// <returns>A collection of posts that match the criteria.</returns>
         [HttpGet("GetPosts/{postId}/{userId}/{searchParam}")]
         public IEnumerable<Post> GetPosts(int postId = 0, int userId = 0, string searchParam = "None") 
         {
@@ -77,10 +91,9 @@ namespace DotnetAPI.Controllers;
         }
 
         /// <summary>
-        /// Retrieves all posts by a specific user.
+        /// Retrieves posts created by the currently authenticated user.
         /// </summary>
-        /// <param name="userId">ID of the user.</param>
-        /// <returns>List of posts by the user.</returns>
+        /// <returns>A collection of posts created by the authenticated user.</returns>
         [HttpGet("GetMyPosts")]
         public IEnumerable<Post> GetMyPosts() 
         {
@@ -90,12 +103,17 @@ namespace DotnetAPI.Controllers;
             return myResult;
         }
 
+        /// <summary>
+        /// Deletes a specified post if it was created by the authenticated user.
+        /// </summary>
+        /// <param name="postId">The ID of the post to delete.</param>
+        /// <returns>Status indicating success or failure of the deletion.</returns>
         [HttpDelete("DeletePost/{postId}")]
         public IActionResult DeletePost(int postId) 
         {
             string sql = @"DELETE FROM DotnetAPISchema.Posts 
                 WHERE PostId = " + postId.ToString() +
-                    "AND UserId = " + this.User.FindFirst("userId")?.Value; 
+                    " AND UserId = " + this.User.FindFirst("userId")?.Value; 
 
             if (_dapper.ExecuteSql(sql)) 
             {
@@ -103,6 +121,5 @@ namespace DotnetAPI.Controllers;
             }
             throw new Exception("Failed to delete the post!");
         }
-    
-
+    }
 }
